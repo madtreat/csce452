59bdcf48116e88cf4a59f6ce307545ad295ab16d
Madison Treat <madtreat@maka.hmc.com>
2014-03-18 12:13:18 -0500

    it is kinda working moar better

--- src/robotarm.cpp
+++ src/robotarm.cpp
@@ -153,6 +153,8 @@ void RobotArm::translateBrush(Link* brush, int newX, int newY)
 	// get Brush
 	int curX = brush->joint.X;
 	int curY = brush->joint.Y;
+   int l2X = links[2]->joint.X;
+   int l2Y = links[2]->joint.Y;
 	// get link lengths (should be 100 and 75 respectively)
    int l1len = links[1]->length;
 	int l2len = links[2]->length;
@@ -161,73 +163,148 @@ void RobotArm::translateBrush(Link* brush, int newX, int newY)
    // total distanced moved in this direction
    int diff = abs(curX - newX);
 		
-   // not really distance, but did not want to sqrt, then square it
-   // is really dist ^ 2 (c^2)
-   int deltaX_2 = pow((newX - links[2]->joint.X), 2);
-   int deltaY_2 = pow((newY - links[2]->joint.Y), 2);
-   long int hyp_2  = deltaX_2 + deltaY_2;
+   int deltaX_2 = 0;
+   int deltaY_2 = 0;
+   long int hyp_2 = 1;
+   int numer = 0;
+   int denom = 1;
 
-   // law of cosines
-   int numer  = pow(l2len, 2) + pow(l3len, 2) - hyp_2;
-   int denom  = 2 * l2len * l3len;
+   int link1X = newX;
+
+   double theta2 = 0;
+   double theta2_deg = 0;
+   double theta3 = 0;
+   double theta3_deg = 0;
+   double phi1 = 0;
+   double phi2 = 0;
+
+   //*
+   while (true)
+   {
+      // */
+      // not really distance, but did not want to sqrt, then square it
+      // is really dist ^ 2 (c^2)
+      deltaX_2 = pow((link1X - links[2]->joint.X), 2);
+      deltaY_2 = pow((newY - links[2]->joint.Y), 2);
+      hyp_2  = deltaX_2 + deltaY_2;
+
+      // law of cosines
+      numer  = pow(l2len, 2) + pow(l3len, 2) - hyp_2;
+      denom  = 2 * l2len * l3len;
+/*
+      if (abs( (double)numer / (double)denom ) > 1.0)
+         link1X++;
+      else
+         break;
+   }
+   // */
+
    //double angle3 = acos( (double) numer / (double) denom );
 
    // law of sines
    //double angle2 = asin( ( sin(angle3) / sqrt(hyp_2) ) * l3len );
    
    
-   /******************************************/
+      /******************************************/
 
-   /****      This is the difference      ****/
-   // The following is team DROP DATABASE's code
-   /******************************************/
-   //Theta3 = cos^-1((-(X3 - X0)^2 - (Y3 - L0)^2 + L3^2 + L2^2) / (L3 * L2))
-	double theta3 = acos( ( -hyp_2 + pow(l2len, 2) + pow(l3len, 2) ) / denom );
-   // == our angle3
-	//Phi2 = cos^-1(((X3 - X0)^2 + (Y3 - L0)^2 - L3^2 + L2^2) / (sqrt((X3 - X0)^2 + (Y3 - L0)^2) * L2))
-	double phi2 = acos( ( hyp_2 + pow(l2len, 2) - pow(l3len, 2) ) / 
-                       ( 2 * l2len * sqrt(hyp_2)));
-	//Phi1 = cos^-1(((X3 - X0)^2 + (Y3 - L0)^2 + L1^2  - (X3 - X0)^2 - (Y3)^2) / (2sqrt((X3 - X0)^2 + (Y3 - L0)^2) * L1))
-	double phi1 = acos( ( hyp_2 + pow(l1len, 2) - deltaX_2 - pow(newY, 2) /
-                       ( 2 * l1len * sqrt(hyp_2))));
-	double theta2 = phi1 - phi2;
+      /****      This is the difference      ****/
+      // The following is team DROP DATABASE's code
+      /******************************************/
+      //Theta3 = cos^-1((-(X3 - X0)^2 - (Y3 - L0)^2 + L3^2 + L2^2) / (L3 * L2))
+      double t3val = ( -hyp_2 + pow(l2len, 2) + pow(l3len, 2) ) / denom;
+      theta3 = acos( t3val );
+      // == our angle3
 
-   double theta2_deg = theta2 * 180.0/PI;
-   double theta3_deg = theta3 * 180.0/PI;
+      //Phi2 = cos^-1(((X3 - X0)^2 + (Y3 - L0)^2 - L3^2 + L2^2) / (sqrt((X3 - X0)^2 + (Y3 - L0)^2) * L2))
+      double p2val =  ( hyp_2 + pow(l2len, 2) - pow(l3len, 2) ) / 
+                        ( 2 * l2len * sqrt(hyp_2));
+      phi2 = acos(p2val);
+      //Phi1 = cos^-1(((X3 - X0)^2 + (Y3 - L0)^2 + L1^2  - (X3 - X0)^2 - (Y3)^2) / (2sqrt((X3 - X0)^2 + (Y3 - L0)^2) * L1))
+
+      double p1val = ( hyp_2 + pow(l1len, 2) - deltaX_2 - pow(newY, 2) /
+                        ( 2 * l1len * sqrt(hyp_2)));
+      phi1 = acos( p1val );
+
+
+      if (abs(t3val) > 1.0 || abs(p2val) > 1.0 )//|| abs(p1val) > 1.0)
+      {
+         link1X++;
+         //translateBrush(brush, ++newX, newY);
+      }
+      else
+      {
+         break;
+      }
    
-   double angle2 = theta2_deg;
-   double angle3 = theta3_deg;
+      theta2 = phi1 - phi2;
+
+      theta2_deg = theta2 * 180.0/PI;
+      theta3_deg = theta3 * 180.0/PI;
+      
+      double angle2 = theta2_deg;
+      double angle3 = theta3_deg;
+   }   
+   
+
+   //*
+   cout << "dist:    "<< hyp_2 << endl;
+   cout << "numer:   "<< numer << endl;
+   cout << "denom:   "<< denom << endl;
+   cout << "div:     "<< (double) numer / (double) denom << endl;
+   // */
+
+   cout << "     theta2 : " << theta2 << endl;
+   cout << "     theta3 : " << theta3 << endl;
+   cout << "       phi1 : " << phi1 << endl;
+   cout << "       phi2 : " << phi2 << endl;
+   cout << "     theta2d: " << theta2_deg << endl;
+   cout << "     theta3d: " << theta3_deg << endl;
+   cout << "sin(theta)2 : " << sin(theta2) << endl;
+   cout << "sin(theta)3 : " << sin(theta3) << endl;
+   cout << "  sin(phi)1 : " << sin(phi1) << endl;
+   cout << "  sin(phi)2 : " << sin(phi2) << endl;
+   cout << "sin(theta)2d: " << sin(theta2_deg) << endl;
+   cout << "sin(theta)3d: " << sin(theta3_deg) << endl;
 
    // assign them
-   /*
-   if (newX < links[1]->joint.range_min)
+   
+   
+   //*
+   if (link1X < links[1]->joint.range_min)
       links[1]->joint.X = links[1]->joint.range_min;
-   else if (newX + OFFSET_X > links[1]->joint.range_max)
+   else if (link1X + OFFSET_X > links[1]->joint.range_max)
       links[1]->joint.X = links[1]->joint.range_max;
    // */
    // need to take the offset into account
-   links[1]->joint.X = newX;
+   links[1]->joint.X = link1X;//newX;
+   links[2]->joint.X = link1X;//newX;
 
    /*
    links[2]->joint.X = links[2]->length * ( cos(theta2)        + sin(theta2) );
-   links[2]->joint.Y = links[2]->joint.Y * ( -1.0*sin(theta2)   + cos(theta2) );
-
-   links[3]->joint.X = links[3]->joint.X * ( cos(theta3)        + sin(theta3) );
-   links[3]->joint.Y = links[3]->joint.Y * ( -1.0*sin(theta3)   + cos(theta3) );
+   links[2]->joint.Y = links[2]->length * ( -1.0*sin(theta2)   + cos(theta2) );
+   links[3]->joint.X = l2X + l2len * ( cos(phi2)        + sin(phi2) );
+   links[3]->joint.Y = l2Y + l2len * ( -1.0*sin(phi2)   + cos(phi2) );
    // */
    // I replaced the above with these to attempt combining theirs with ours
-   rotateJoint(links[2], Motion(), (int) theta2);
-   rotateJoint(links[3], Motion(), (int) 90-theta3);
+   //rotateJoint(links[2], Motion(), (int) cos(theta2)     + sin(theta2));
+   //rotateJoint(links[3], Motion(), (int) -1.0*sin(theta3)+ cos(90-theta3));
+   //links[2]->joint.X += links[2]->length * sin(theta2_deg);
+   //links[2]->joint.Y += links[2]->length * cos(theta2_deg);
+
+   links[3]->joint.X = links[2]->joint.X + l2len * sin(phi2);//theta3_deg);
+   if (cos(phi2) > 0.0)
+   {
+      links[3]->joint.Y = links[2]->joint.Y + l2len * -cos(phi2);//theta3_deg);
+   }
+   else
+   {
+      links[3]->joint.Y = links[2]->joint.Y + l2len * cos(phi2);//theta3_deg);
+   }
+
 
    links[4]->joint.X = newX;
    links[4]->joint.Y = newY;
 
-   /*
-   cout << "dist:    "<< hyp_2 << endl
-         << "numer:   "<< numer << endl
-         << "denom:   "<< denom << endl
-         << "div:     "<< (double) numer / (double) denom << endl;
-   // */
 
    cout << "angle 2R: " << theta2 << endl;
    cout << "angle 3R: " << theta3 << endl;
