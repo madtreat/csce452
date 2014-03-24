d71ff8d0aa1885a3137ab62569269586dea5d197
Violeta Torres <violeta@violeta-VirtualBox.(none)>
2014-03-24 15:21:09 -0500

    added arguments and parse argument

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,59 @@ int main(int argc, char* argv[])
    QApplication app(argc, argv);
 
    AppType    type = SERVER;
-   QString address = "localhost";
-   int        port = 51315;
-   int       delay = 2;
-   
+   QString address = "";
+   int        port = 0;
+   int       delay = 0;
+   bool server = false; 
+   bool client = false; 
    // parse args
+  int c;
+
+  // get command line args
+  while((c = getopt (argc, argv, "s:h:p:c:d:")) != -1)
+    switch(c) {
+      case 's': // run as server using port 
+        server = true; 
+        break;
+
+      case 'c': // run client using host n port 
+        client = true; 
+        break;
+
+      case 'd': // initialize delay
+        delay = atoi(optarg);
+        break;
+
+      case 'p': // port 
+        port = atoi(optarg);
+	break; 
+
+      case 'h': // set up the host 
+	 address = optarg; 
+	break; 
+      
+	default:
+        abort();
+    }
+	
+    if (server && client)
+	{
+	 printUsage();
+	return 1;
+	}
+
+    if(server && port == 0)
+	{
+	printUsage();
+	return 1;
+	}
+
+    if(client && (port == 0 || address == "" ))
+	{
+	printUsage();
+	return 1;
+	}
+
    ConnectionInfo info;
    info.type   = type;
    info.host   = QHostAddress(address);
@@ -28,6 +76,8 @@ int main(int argc, char* argv[])
    info.delay  = delay;
 
    Window w(info);
+
+
    w.show();
 
    cout << "Starting PaintBot..." << endl;
