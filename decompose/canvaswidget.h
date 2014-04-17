afb55d0ecad221bd989929d5c8a85ce7c7e5bbb5
mperry14 <mattperry@tamu.edu>
2014-04-17 13:37:51 -0500

    Cleanedcanvaswidget

--- decompose/canvaswidget.h
+++ decompose/canvaswidget.h
@@ -21,18 +21,6 @@ public:
    ~CanvasWidget();
    
    QSize sizeHint() const;
-   
-	//TODO: Delete. cause moved to manager
-	/*
-	void printCarLocs() const;
-   void printLightLocs() const;
-
-   Cars     getCars() const {return cars;}
-   Lights   getLights() const {return lights;}
-
-   Car      getCar(int i) const {if (i >= cars.size()) return Car(); else return cars[i];}
-   Light    getLight(int i) const {if (i >= lights.size()) return Light(); else return lights[i];}
-	//*/
 
 signals:
    void jointsChanged();
@@ -40,16 +28,6 @@ signals:
 
 public slots:
    void animate();
-   
-	//TODO: Delete. cause moved to manager
-	/*
-	void addNewCar(Car car);
-   void addNewLight(Light light);
-   void deleteCar(int car);
-   void deleteLight(int light);
-   void updateCarPos(int car, int newX, int newY, bool directMapping);
-   void updateLightPos(int light, int newX, int newY);
-	//*/
 
 protected:
    void paintEvent(QPaintEvent *event);
@@ -58,12 +36,6 @@ protected:
 
 private:
    Canvas*  canvas;
-	
-	//TODO: Delete. cause moved to manager
-	/*
-   Cars     cars;    // list of cars
-   Lights   lights;  // list of lights
-	*/
 };
 
 #endif
